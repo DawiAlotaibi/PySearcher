f1dcd07b469dc24018047cd737150cd54cf1490e
DawiAlotaibi <dawialotaibi47@gmail.com>
2023-07-07 16:25:50 +0300

    feat: Expanded GUI

--- file_searcher.py
+++ file_searcher.py
@@ -1,11 +1,163 @@
 import os
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import ttk
 from tkinter import messagebox
 from docx import Document
 import json
 from PyPDF2 import PdfReader
 from pptx import Presentation
+
+
+class App(ttk.Frame):
+    def __init__(self, parent):
+        ttk.Frame.__init__(self)
+        self.treeview_data = []
+        self.popup = None
+        self.directory_label = None
+        self.directory_frame = None
+        self.directory_entry = None
+        self.browse_button = None
+        self.search_button = None
+        self.results_label = None
+        self.results_text = None
+        self.directory_var = None
+        self.exact_match_checkbox = None
+        self.exact_match_var = None
+        self.case_sensitive_checkbox = None
+        self.case_sensitive_var = None
+        self.search_entry = None
+        self.search_label = None
+        self.setup_widgets()
+
+    def setup_widgets(self):
+        # self.search_label = tk.Label(window, text="Search String:")
+        # self.search_label.pack()
+        self.search_label = ttk.Label(
+            self,
+            text="Search String:",
+            justify="center",
+            font=("-size", 10, "-weight", "bold"),
+        )
+        self.search_label.grid(row=0, column=0, padx=5, pady=(0, 10), sticky="ew")
+
+        # self.search_entry = tk.Entry(window, width=50)
+        # self.search_entry.pack()
+        self.search_entry = ttk.Entry(self)
+        self.search_entry.insert(0, "Entry")
+        self.search_entry.grid(row=0, column=1, padx=5, pady=(0, 10), sticky="ew")
+
+        self.case_sensitive_var = tk.BooleanVar()
+        # self.case_sensitive_checkbox = tk.Checkbutton(window, text="Case Sensitive", variable=self.case_sensitive_var)
+        # self.case_sensitive_checkbox.pack()
+        self.case_sensitive_checkbox = ttk.Checkbutton(
+            self, text="Case Sensitive", variable=self.case_sensitive_var
+        )
+        self.case_sensitive_checkbox.grid(row=0, column=2, padx=5, pady=(0, 10), sticky="ew")
+
+        self.exact_match_var = tk.BooleanVar()
+        # self.exact_match_checkbox = tk.Checkbutton(window, text="Exact Match", variable=self.exact_match_var)
+        # self.exact_match_checkbox.pack()
+        self.exact_match_checkbox = ttk.Checkbutton(
+            self, text="Exact Match", variable=self.exact_match_var
+        )
+        self.exact_match_checkbox.grid(row=0, column=3, padx=5, pady=(0, 10), sticky="ew")
+
+        self.directory_var = tk.StringVar()
+        # self.directory_label = tk.Label(window, text="Search Directory:")
+        # self.directory_label.pack()
+        self.directory_label = ttk.Label(
+            self,
+            text="Search Directory:",
+            justify="center",
+            font=("-size", 10, "-weight", "bold"),
+        )
+        self.directory_label.grid(row=1, column=0, padx=5, pady=(0, 10), sticky="ew")
+
+        # self.directory_frame = tk.Frame(window)
+        # self.directory_entry = tk.Entry(self.directory_frame, textvariable=self.directory_var, width=40)
+        # self.directory_entry.pack(side=tk.LEFT)
+        # self.directory_frame = ttk.Frame(self, padding=(0, 0, 0, 10))
+        # self.directory_frame.grid(row=1, column=1, padx=5, pady=(0, 10), sticky="ew", rowspan=3)
+        self.directory_entry = ttk.Entry(self, textvariable=self.directory_var, width=40)
+        self.directory_entry.grid(row=1, column=1, padx=5, pady=(0, 10), sticky="ew")
+        self.browse_button = ttk.Button(self, text="Browse", command=browse_directory)
+        self.browse_button.grid(row=1, column=2, padx=5, pady=(0, 10), sticky="ew")
+        self.search_button = ttk.Button(self, text="Search", command=search_files)
+        self.search_button.grid(row=2, column=1, padx=5, pady=(30, 10), sticky="ew")
+
+        # self.browse_button = tk.Button(self.directory_frame, text="Browse", command=browse_directory)
+        # self.browse_button.pack(side=tk.LEFT)
+        #
+        # self.directory_frame.pack()
+        #
+        # self.search_button = tk.Button(window, text="Search", command=search_files)
+        # self.search_button.pack()
+        #
+        # self.results_label = tk.Label(window, text="Results:")
+        # self.results_label.pack()
+        #
+        self.results_text = tk.Text(window, width=60, height=15)
+        # self.results_text.pack()
+
+    def clear_treeview(self):
+        self.popup.treeview.delete(*self.popup.treeview.get_children())
+        self.treeview_data.clear()
+        self.popup.destroy()
+
+    def show_popup(self, original_search_string):
+        self.popup = tk.Toplevel()
+        self.popup.title("Results")
+        self.popup.protocol("WM_DELETE_WINDOW", self.clear_treeview)
+        self.popup.searchString = ttk.Label(
+            self,
+            text=original_search_string,
+            justify="center",
+            font=("-size", 10, "-weight", "bold"),
+        )
+        self.search_label.grid(row=0, column=0, padx=5, pady=(0, 10), sticky="ew")
+
+        self.popup.scrollbar = ttk.Scrollbar(self.popup)
+        self.popup.scrollbar.pack(side="right", fill="y")
+        self.popup.treeview = ttk.Treeview(
+            self.popup,
+            selectmode="browse",
+            yscrollcommand=self.popup.scrollbar.set,
+            columns=(1, 2),
+            height=10,
+        )
+        self.popup.treeview.pack(expand=True, fill="both")
+        self.popup.scrollbar.config(command=self.popup.treeview.yview)
+
+        # Treeview columns
+        self.popup.treeview.column("#0", anchor="w", width=120)
+        self.popup.treeview.column(2, anchor="w", width=120)
+        self.popup.treeview.column(1, anchor="w", width=120)
+
+        # Treeview headings
+        self.popup.treeview.heading("#0", text="Path", anchor="center")
+        self.popup.treeview.heading(1, text="File", anchor="center")
+        self.popup.treeview.heading(2, text="Line/Page", anchor="center")
+        # # Add content to the popup window
+        # label = ttk.Label(self.popup, text="This is a popup window!")
+        # label.pack(padx=10, pady=10)
+        #
+        # # Add a button to close the popup
+        # close_button = ttk.Button(self.popup, text="Close", command=self.popup.destroy)
+        # close_button.pack(pady=10,fill="both", expand=True)
+
+        for item in self.treeview_data:
+            print(item)
+            self.popup.treeview.insert(
+                parent=item[0], index="end", iid=item[1], text=item[2], values=item[3]
+            )
+            if item[0] == "" or item[1] in {8, 21}:
+                self.popup.treeview.item(item[1], open=True)  # Open parents
+        self.popup.update()
+        self.popup.minsize(self.popup.winfo_width(), self.popup.winfo_height())
+        x_coordinate = int((self.popup.winfo_screenwidth() / 2) - (self.popup.winfo_width() / 2))
+        y_coordinate = int((self.popup.winfo_screenheight() / 2) - (self.popup.winfo_height() / 2))
+        self.popup.geometry("+{}+{}".format(x_coordinate, y_coordinate - 20))
 
 
 def check_file_contents(filename, search_string, case_sensitive=True, exact_match=False):
@@ -18,6 +170,7 @@ def check_file_contents(filename, search_string, case_sensitive=True, exact_matc
 
     if file_extension == '.docx':
         doc = Document(filename)
+        line_number = 1
         for paragraph in doc.paragraphs:
             if not case_sensitive:
                 paragraph_text = paragraph.text.lower()
@@ -26,12 +179,21 @@ def check_file_contents(filename, search_string, case_sensitive=True, exact_matc
 
             if exact_match:
                 if search_string == paragraph_text:
-                    results_text.insert(tk.END,
-                                        f"Found '{original_search_string}' in {filename} - Line: {paragraph.text}\n")
+                    # app.results_text.insert(tk.END,
+                    #                         f"Found '{original_search_string}' in {filename} - Line: {line_number}\n")
+                    actualFileName = os.path.basename(filename)
+                    app.treeview_data.append(
+                        ("", len(app.treeview_data) + 1, filename, (actualFileName, f"Line: {line_number}")))
             else:
                 if search_string in paragraph_text:
-                    results_text.insert(tk.END,
-                                        f"Found '{original_search_string}' in {filename} - Line: {paragraph.text}\n")
+                    # app.results_text.insert(tk.END,
+                    #                         f"Found '{original_search_string}' in {filename} - Line: {line_number}\n")
+                    actualFileName = os.path.basename(filename)
+                    app.treeview_data.append(
+                        ("", len(app.treeview_data) + 1, filename, (actualFileName, f"Line: {line_number}")))
+
+            line_number += 1
+
 
     elif file_extension == '.py':
         with open(filename, 'r') as file:
@@ -41,12 +203,20 @@ def check_file_contents(filename, search_string, case_sensitive=True, exact_matc
 
                 if exact_match:
                     if search_string == line.strip():
-                        results_text.insert(tk.END,
-                                            f"Found '{original_search_string}' in {filename} - Line {line_number}: {line.strip()}\n")
+                        # app.results_text.insert(tk.END,
+                        #                         f"Found '{original_search_string}' in {filename} - Line {line_number}: {line.strip()}\n")
+                        actualFileName = os.path.basename(filename)
+                        app.treeview_data.append(("", len(app.treeview_data) + 1, filename,
+                                                  (actualFileName, f"Line: {line_number}")))
+
                 else:
                     if search_string in line:
-                        results_text.insert(tk.END,
-                                            f"Found '{original_search_string}' in {filename} - Line {line_number}: {line.strip()}\n")
+                        # app.results_text.insert(tk.END, f"Found '{original_search_string}' in {filename} - Line {
+                        # line_number}: {line.strip()}\n")
+                        actualFileName = os.path.basename(filename)
+                        app.treeview_data.append(("", len(app.treeview_data) + 1, filename,
+                                                  (actualFileName, f"Line: {line_number}")))
+
 
 
     elif file_extension == '.txt':
@@ -57,14 +227,20 @@ def check_file_contents(filename, search_string, case_sensitive=True, exact_matc
 
                 if exact_match:
                     if search_string == line.strip():
-                        results_text.insert(tk.END,
-                                            f"Found '{original_search_string}' in {filename} - Line {line_number}: {line.strip()}\n")
+                        # app.results_text.insert(tk.END,
+                        #                         f"Found '{original_search_string}' in {filename} - Line {line_number}: {line.strip()}\n")
+                        actualFileName = os.path.basename(filename)
+                        app.treeview_data.append(("", len(app.treeview_data) + 1, filename,
+                                                  (actualFileName, f"Line: {line_number}")))
                 else:
                     if search_string in line:
-                        results_text.insert(tk.END,
-                                            f"Found '{original_search_string}' in {filename} - Line {line_number}: {line.strip()}\n")
+                        # app.results_text.insert(tk.END,
+                        #                         f"Found '{original_search_string}' in {filename} - Line {line_number}: {line.strip()}\n")
+                        actualFileName = os.path.basename(filename)
+                        app.treeview_data.append(("", len(app.treeview_data) + 1, filename,
+                                                  (actualFileName, f"Line: {line_number}")))
 
-    elif file_extension == '.json':
+    elif file_extension == '.json':  # I need to find a way to print the line here
         with open(filename, 'r') as file:
             data = json.load(file)
             if not case_sensitive:
@@ -74,10 +250,16 @@ def check_file_contents(filename, search_string, case_sensitive=True, exact_matc
 
             if exact_match:
                 if search_string == data_str:
-                    results_text.insert(tk.END, f"Found '{original_search_string}' in {filename}\n")
+                    # app.results_text.insert(tk.END, f"Found '{original_search_string}' in {filename}\n")
+                    actualFileName = os.path.basename(filename)
+                    app.treeview_data.append(("", len(app.treeview_data) + 1, filename,
+                                              (actualFileName, data_str)))
             else:
                 if search_string in data_str:
-                    results_text.insert(tk.END, f"Found '{original_search_string}' in {filename}\n")
+                    # app.results_text.insert(tk.END, f"Found '{original_search_string}' in {filename}\n")
+                    actualFileName = os.path.basename(filename)
+                    app.treeview_data.append(("", len(app.treeview_data) + 1, filename,
+                                              (actualFileName, data_str)))
 
     elif file_extension == '.pdf':
         with open(filename, 'rb') as file:
@@ -89,13 +271,15 @@ def check_file_contents(filename, search_string, case_sensitive=True, exact_matc
 
                 if exact_match:
                     if search_string == page_content.strip():
-                        results_text.insert(tk.END,
-                                            f"Found '{original_search_string}' in {filename} - Page {page_number}\n")
+                        actualFileName = os.path.basename(filename)
+                        app.treeview_data.append(("", len(app.treeview_data) + 1, filename,
+                                                  (actualFileName, f"Page: {page_number}")))
+
                 else:
                     if search_string in page_content:
-                        results_text.insert(tk.END,
-                                            f"Found '{original_search_string}' in {filename} - Page {page_number}\n")
-
+                        actualFileName = os.path.basename(filename)
+                        app.treeview_data.append(("", len(app.treeview_data) + 1, filename,
+                                                  (actualFileName, f"Page: {page_number}")))
     elif file_extension == '.pptx':
         prs = Presentation(filename)
         for slide_number, slide in enumerate(prs.slides, start=1):
@@ -110,87 +294,61 @@ def check_file_contents(filename, search_string, case_sensitive=True, exact_matc
 
             if exact_match:
                 if search_string == slide_text.strip():
-                    results_text.insert(tk.END,
-                                        f"Found '{original_search_string}' in {filename} - Slide {slide_number}\n")
+                    actualFileName = os.path.basename(filename)
+                    app.treeview_data.append(("", len(app.treeview_data) + 1, filename,
+                                              (actualFileName, f"Page: {slide_number}")))
             else:
                 if search_string in slide_text:
-                    results_text.insert(tk.END,
-                                        f"Found '{original_search_string}' in {filename} - Slide {slide_number}\n")
-
+                    actualFileName = os.path.basename(filename)
+                    app.treeview_data.append(("", len(app.treeview_data) + 1, filename,
+                                              (actualFileName, f"Page: {slide_number}")))
     else:
-        results_text.insert(tk.END, f"Unsupported file format: {file_extension}\n")
+        app.results_text.insert(tk.END, f"Unsupported file format: {file_extension}\n")
 
 
 def browse_directory():
     selected_directory = filedialog.askdirectory()
-    directory_var.set(selected_directory)
+    app.directory_var.set(selected_directory)
 
 
 def search_files():
-    search_string = search_entry.get().strip()
+    search_string = app.search_entry.get().strip()
     if not search_string:
         messagebox.showwarning("Empty Search String", "Please enter a search string.")
         return
 
-    case_sensitive = case_sensitive_var.get()
-    exact_match = exact_match_var.get()
+    case_sensitive = app.case_sensitive_var.get()
+    exact_match = app.exact_match_var.get()
 
-    search_directory = directory_var.get()
+    search_directory = app.directory_var.get()
     if not search_directory:
         messagebox.showwarning("No Directory Selected", "Please select a directory to search.")
         return
 
     script_file = os.path.normpath(os.path.abspath(__file__))
 
-    results_text.delete(1.0, tk.END)
-
     for filename in os.listdir(search_directory):
         file_path = os.path.normpath(os.path.join(search_directory, filename))
 
         if os.path.isfile(file_path) and not filename.startswith('~$') and file_path != script_file:
             check_file_contents(file_path, search_string, case_sensitive, exact_match)
+    app.show_popup(search_string)
 
 
 # Create the main window
 window = tk.Tk()
 window.title("File Searcher")
-window.geometry("500x400")
 
-# Create and place widgets
-search_label = tk.Label(window, text="Search String:")
-search_label.pack()
+window.tk.call("source", "azure.tcl")
+window.tk.call("set_theme", "dark")
 
-search_entry = tk.Entry(window, width=50)
-search_entry.pack()
+app = App(window)
+app.pack(fill="both", expand=True)
 
-case_sensitive_var = tk.BooleanVar()
-case_sensitive_checkbox = tk.Checkbutton(window, text="Case Sensitive", variable=case_sensitive_var)
-case_sensitive_checkbox.pack()
-
-exact_match_var = tk.BooleanVar()
-exact_match_checkbox = tk.Checkbutton(window, text="Exact Match", variable=exact_match_var)
-exact_match_checkbox.pack()
-
-directory_var = tk.StringVar()
-directory_label = tk.Label(window, text="Search Directory:")
-directory_label.pack()
-
-directory_frame = tk.Frame(window)
-directory_entry = tk.Entry(directory_frame, textvariable=directory_var, width=40)
-directory_entry.pack(side=tk.LEFT)
-
-browse_button = tk.Button(directory_frame, text="Browse", command=browse_directory)
-browse_button.pack(side=tk.LEFT)
-
-directory_frame.pack()
-
-search_button = tk.Button(window, text="Search", command=search_files)
-search_button.pack()
-
-results_label = tk.Label(window, text="Results:")
-results_label.pack()
-
-results_text = tk.Text(window, width=60, height=15)
-results_text.pack()
+window.update()
+window.minsize(window.winfo_width(), window.winfo_height())
+x_coordinate = int((window.winfo_screenwidth() / 2) - (window.winfo_width() / 2))
+y_coordinate = int((window.winfo_screenheight() / 2) - (window.winfo_height() / 2))
+window.geometry("+{}+{}".format(x_coordinate, y_coordinate - 20))
 
 window.mainloop()
